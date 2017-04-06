d910ebc322a34709fef39d7fa3a84a36d639e721
fheitmann <frederic.andre@gmail.com>
2017-04-06 12:37:58 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export * from './src/option.interface';
-export * from './src/select.component';
+export * from './src/option.interface.ts';
+export * from './src/select.component.ts';
 export declare class SelectModule {
 }
