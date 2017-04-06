bc7db365c3daee1cad9f79099195f70469f4a820
fheitmann <frederic.andre@gmail.com>
2017-04-06 10:28:01 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const autoprefixer = require('autoprefixer')
 module.exports = {
- entry: './index.ts',
+ entry: './src/index.ts',
  output: {
    filename: 'bundle.js',
    path: __dirname
