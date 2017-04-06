fe6fccf49ef519ddeaa0f0dfd4c6030e02425867
fheitmann <frederic.andre@gmail.com>
2017-04-06 10:26:59 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,22 @@
 const path = require('path')
 const webpack = require('webpack')
 const autoprefixer = require('autoprefixer')
-
 module.exports = {
-  entry: {
-    "ignore":['./theming/index.ts'],
-    "bundle":['./src/index.ts']
-  },
-  output: {
-    path: path.join(__dirname, 'release'),
-    filename: '[name].js',
-    libraryTarget:"umd",
-    publicPath: ''
-  },
-  resolve: {
-    extensions:[".js", ".ts", ".tsx","", ".webpack.js", ".web.js", ".scss"]
-  },
-  plugins: [
-     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: {
-        except: ['require', 'export', '$super']
-      },
-      compress: {
-        warnings: false,
-        sequences: true,
-        dead_code: true,
-        conditionals: true,
-        booleans: true,
-        unused: true,
-        if_return: true,
-        join_vars: true,
-        drop_console: true
-      }
-    })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        loaders: ['ts'],
-        include: [path.join(__dirname, 'src'),path.join(__dirname, 'theming')]
-      }
-    ]
-  }
+ entry: './index.ts',
+ output: {
+   filename: 'bundle.js',
+   path: __dirname
+ },
+ module: {
+   rules: [
+     {
+       test: /\.tsx?$/,
+       loader: 'ts-loader',
+       exclude: /node_modules/,
+     },
+   ]
+ },
+ resolve: {
+   extensions: [".tsx", ".ts", ".js"]
+ },
 };
